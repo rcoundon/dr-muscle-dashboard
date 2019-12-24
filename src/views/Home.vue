82e2c7cc7f24321d4535ec0b9a62bb1a67476502
Ross Coundon <ross.coundon@leadent.digital>
2019-12-24 17:50:06 +0000

    Add loading text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container is-fluid">
     <b-loading
-      :is-full-page="true"
+      :is-full-page="false"
       :active.sync="isLoading"
       :can-cancel="false"
     />
-
+    <div v-if="isLoading">
+      <p class="is-size-4 is-primary has-text-weight-semibold">Loading full history...</p>
+      <p class="is-size-4 is-primary ">If you've been working out a while</p>
+      <p class="is-size-4 is-primary ">This could take up to 30s (but it's worth it!)</p>
+      <p class="is-size-4 processing">Downloading<span>.</span><span>.</span><span>.</span></p>
+    </div>
     <div class="columns">
       <div class="column">
       <b-field style="padding-top: 2em; padding-left: 1em">
@@ -128,3 +133,75 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+body {
+  background: grey;
+  font-family: helvetica;
+  font-size: 72px;
+}
+@keyframes blink {
+  /**
+     * At the start of the animation the dot
+     * has an opacity of .2
+     */
+  0% {
+    opacity: 0.2;
+  }
+  /**
+     * At 20% the dot is fully visible and
+     * then fades out slowly
+     */
+  20% {
+    opacity: 1;
+  }
+  /**
+     * Until it reaches an opacity of .2 and
+     * the animation can start again
+     */
+  100% {
+    opacity: 0.2;
+  }
+}
+
+.processing span {
+  /**
+     * Use the blink animation, which is defined above
+     */
+  animation-name: blink;
+  /**
+     * The animation should take 1.4 seconds
+     */
+  animation-duration: 1.4s;
+  /**
+     * It will repeat itself forever
+     */
+  animation-iteration-count: infinite;
+  /**
+     * This makes sure that the starting style (opacity: .2)
+     * of the animation is applied before the animation starts.
+     * Otherwise we would see a short flash or would have
+     * to set the default styling of the dots to the same
+     * as the animation. Same applies for the ending styles.
+     */
+  animation-fill-mode: both;
+}
+
+.processing span:nth-child(2) {
+  /**
+     * Starts the animation of the third dot
+     * with a delay of .2s, otherwise all dots
+     * would animate at the same time
+     */
+  animation-delay: 0.2s;
+}
+
+.processing span:nth-child(3) {
+  /**
+     * Starts the animation of the third dot
+     * with a delay of .4s, otherwise all dots
+     * would animate at the same time
+     */
+  animation-delay: 0.4s;
+}
+</style>
