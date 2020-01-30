def57c840b90a0e01e4d1b503be1ef71c52cb217
Ross Coundon <ross.coundon@leadent.digital>
2020-01-30 09:54:24 +0000

    Auto-deploy bumped version number

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 const { BugsnagBuildReporterPlugin } = require('webpack-bugsnag-plugins');
 const version = require('./package.json').version;
+const webpack = require('webpack');
 
 module.exports = {
   productionSourceMap: true,
@@ -16,13 +17,30 @@ module.exports = {
   // }
   configureWebpack: {
     optimization: {
-      moduleIds: 'hashed',
+      runtimeChunk: 'single',
       splitChunks: {
-        minSize: 50000,
-        maxSize: 250000
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`;
+            }
+          }
+        }
       }
     },
     plugins: [
+      new webpack.HashedModuleIdsPlugin(),
       new CompressionPlugin({
         test: /\.js(\?.*)?$/i,
         filename: '[path].gz[query]',
